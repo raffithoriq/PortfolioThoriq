b71d1530f340e21ffbe2911ac2df9f8fd3b7fad4
Thoriq <thoriqibb@gmail.com>
2025-08-08 16:19:28 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Achmad Roffi Thoriq",
+  description: "Created with v0",
+  generator: "v0.dev",
+  icons: {
+    icon: "/images/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -27,5 +30,5 @@ html {
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
